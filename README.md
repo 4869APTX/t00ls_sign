d2383496faeb99891aa5eb3e35f521ae9321b266
melonrind <yhfaptx4869@gmail.com>
2020-05-12 23:00:42 +0800

    debug ....

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # T00ls sign
 使用 GitHub Actions 每天自动执行 T00ls 签到
 参(chao)考(xi)自 https://github.com/xirikm/hostloc-auto-get-points
- 
+
 ## 使用说明
 
 Fork 本仓库，然后点击你的仓库右上角的 Settings，找到 Secrets 这一项，添加两个秘密环境变量。
 
+>该项目fork自 `hang333/t00ls_sign`, ,将server酱推送修改为企业微信推送，添加 `WECHAT_CORPID` 企业微信ID， `WECHAT_SECRET` 企业微信应用key  `AGENT_ID` 企业微信应用id `WX_USER`推送人。 by melonrind
 
 
 其中 `T00LS_USERNAME` 存放你在 T00ls 的帐户名，`T00LS_PASSWORD` 存放你的帐户密码， `T00LS_MD5` 在存放的密码是 md5 时设置为 `True`， `T00LS_QID` 存放你的登录问题ID，`T00LS_QANS` 存放你的登录问题答案， `T00LS_SCKEY` 则存放Server酱申请的skey。
@@ -17,5 +18,7 @@ Fork 本仓库，然后点击你的仓库右上角的 Settings，找到 Secrets 
 最后在你这个 Fork 的仓库内随便改点什么（比如给 README 文件删掉或者增加几个字符）提交一下手动触发一次 GitHub Actions 就可以了 **（重要！！！测试发现在 Fork 的仓库上 GitHub Actions 的定时任务不会自动执行，必须要手动触发一次后才能正常工作）** 。
 
 仓库内包含的 GitHub Actions 配置文件会在每天国际标准时间 17 点（北京时间凌晨 1 点）自动执行获取积分的脚本文件，你也可以通过 `Push` 操作手动触发执行（测试发现定时任务的执行可能有 5 到 10 分钟的延迟，属正常现象，耐心等待即可）。
+> 修改至东八区早上11点
+
 
 **注意：** 为了实现某个链接/帐户访问出错时不中断程序继续尝试下一个，GitHub Actions 的状态将永远是“通过”（显示绿色的✔），请自行检查 GitHub Actions 日志 `T00ls Sign` 项的输出确定程序执行情况。
--- t00ls_sign.py
+++ t00ls_sign.py
@@ -40,7 +40,7 @@ password = os.environ['T00LS_PASSWORD']  # 明文密码或密码MD5
 password_hash = ("T00LS_MD5" in os.environ) and os.environ['T00LS_MD5']=='True' or False  # 密码为md5时设置为True
 questionid = ("T00LS_QID" in os.environ) and os.environ['T00LS_QID'] or ''  # 问题ID，参考上面注释，没有可不填
 answer = ("T00LS_QANS" in os.environ) and os.environ['T00LS_QANS'] or ''   # 问题答案，没有可不填
-# SCKEY = ("T00LS_SCKEY" in os.environ) and os.environ['T00LS_SCKEY'] or '' #Server酱申请的skey
+
 
 def Req(url,method='get',payload=None):
     if method == 'get':
@@ -107,14 +107,14 @@ def get_formhash(session):
     res = session.get(url=url_login, headers=headers)
     formhash_1 = re.findall('value=\"[0-9a-f]{8}\"', res.text)
     formhash = re.findall('[0-9a-f]{8}', formhash_1[0])[0]
-#    print(formhash)
+    # print(formhash)
     time.sleep(1)
     return formhash
 def get_current_user(session):
     current_user = re.findall('<a href="members-profile-[\d+].*\.html" target="_blank">{username}</a>'.format(username=username), session)
-    print(''.join(current_user))
+    # print(''.join(current_user))
     cuser = re.findall('[\d+]{4,5}', ''.join(current_user))[0]
-    print("用户ID:"+cuser)
+    # print("用户ID:"+cuser)
     return cuser
 def login_t00ls(session):
     formhash=get_formhash(session)
@@ -122,6 +122,7 @@ def login_t00ls(session):
         passwords = password
     else:
         passwords = hashlib.md5(password.encode('utf-8')).hexdigest()
+    # print(passwords)
     data = {
         'username': username,
         'password': passwords,                                 
@@ -135,27 +136,28 @@ def login_t00ls(session):
 
     censored_data = data.copy()
     censored_data['password'] = '***'
-    print(censored_data)
+    # print(censored_data)
 
     headers['Content-Type'] = 'application/x-www-form-urlencoded'
     headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     headers['Referer'] = 'https://www.t00ls.net/login.html'
     res = session.post(url=url_login, headers=headers, data=data)
-    #print(res.headers)
+    # print(res.text)
     time.sleep(1)
-    #res2=session.get("https://www.t00ls.net/checklogin.html");
-    #print(res2.text)
+    res2=session.get("https://www.t00ls.net/checklogin.html")
+    # print(res2.text)
     return res, formhash
 def get_formhash_1(session):
     res = session.get(url=url_checklogin, headers=headers)
     #print("检查登录："+res.text)
     uid = get_current_user(res.text)
+    # print(res.text)
     #formhash = re.findall('[0-9a-f]{8}', res.text)[0]
     formhash=re.findall(re.compile('formhash=(.*?)">'), res.text)[0]#20200318修复来源不正确问题
     return formhash, uid
 def signin_t00ls(session):
     formhash, uid = get_formhash_1(session)
-    print("formhash:"+formhash+"----", "用户ID:"+uid )
+    # print("formhash:"+formhash+"----", "用户ID:"+uid )
     data = {
         'formhash': formhash,
         'signsubmit': 'apply'
@@ -171,7 +173,7 @@ def main():
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), res_signin.text)
     datamsg={"text":"T00ls签到成功！","desp":res_signin.text}
     if "success" in res_signin.text:
-        message = 'T00ls签到成功'
+        message = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '  T00ls签到成功'  
         PushWechatMessage(WX_USER,message,GetWechatKey())
         
 def main_handler(event, context):
